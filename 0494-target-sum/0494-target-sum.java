3a4e967f6a46cfdd35c95bbd6510849f3979d019
SCT <63411236+saichandutammineni@users.noreply.github.com>
2026-03-03 00:45:17 -0800

    Time: 2226 ms (5.01%), Space: 190.9 MB (5.97%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,6 +1,5 @@
 class Solution {
     private void getWays(int[] nums, int target, List<List<Integer>> res, int index, int sum, List<Integer> ll){
-        
         if(sum==target && index==nums.length){
             res.add(new ArrayList<>(ll));
             return;
@@ -9,37 +8,15 @@ class Solution {
             return;
         }
 
-        // if(sum>target){
-            ll.add(index);
-            getWays(nums, target, res, index+1, sum-(2*nums[index]), ll);
-            // if(ll.size()>0){
-            ll.remove(ll.size()-1);
-            // }
-        // }
-        getWays(nums, target, res, index+1, sum, ll);
-        // if(ll.size()>0){
-        //     ll.remove(ll.size()-1);
-        // }
+        ll.add(index);
+        getWays(nums, target, res, index+1, sum-(nums[index]), ll);
+        ll.remove(ll.size()-1);
+        getWays(nums, target, res, index+1, sum+nums[index], ll);
     }
     public int findTargetSumWays(int[] nums, int target) {
-        // int sum=0, zeroCount=0;
-        // for(int i=0;i<nums.length;i++){
-        //     if(nums[i]==0){
-        //         zeroCount++;
-        //     }
-        //     else{
-        //     sum+=nums[i];
-        //     }
-        // }
-         int sum=0;
-        for(int i=0;i<nums.length;i++){
-           
-            sum+=nums[i];
-        }
         List<List<Integer>> res=new ArrayList<>();
         List<Integer> ll=new ArrayList<>();
-        getWays(nums, target, res, 0, sum, ll);
+        getWays(nums, target, res, 0, 0, ll);
         return res.size();
-        // return zeroCount>0 ? (int)Math.pow(2, zeroCount)* res.size(): res.size();
     }
 }
