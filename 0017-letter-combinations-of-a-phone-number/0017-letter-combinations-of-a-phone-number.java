35d50809b1471b4db19bbdc88cbe292327168060
SCT <63411236+saichandutammineni@users.noreply.github.com>
2026-03-06 21:33:41 -0800

    Time: 0 ms (100%), Space: 43.5 MB (64.02%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,36 +1,37 @@
 class Solution {
-    private String getLetters(char num){
-        switch (num) {
-    case '2': return "abc";
-    case '3': return "def";
-    case '4': return "ghi";
-    case '5': return "jkl";
-    case '6': return "mno";
-    case '7': return "pqrs";
-    case '8': return "tuv";
-    case '9': return "wxyz";
-    default: return "";
-}
+    private String getChars(char num){
+        switch(num){
+            case '2': return "abc";
+            case '3': return "def";
+            case '4': return "ghi";
+            case '5': return "jkl";
+            case '6': return "mno";
+            case '7': return "pqrs";
+            case '8': return "tuv";
+            case '9': return "wxyz";
+
+            default: return "";
+
+        }
     }
 
-    private void getCombs(List<String> res, String digits, StringBuilder sb){
-        if(sb.length()==digits.length()){
-            res.add(sb.toString());
+    private void getCombs(List<String> res, StringBuilder sb, String digits, int index){
+        if(digits.length()==index){
+            res.add(new String(sb));
             return;
         }
 
-        String str=getLetters(digits.charAt(sb.length()));
-        for(int i=0;i<str.length();i++){
-            sb.append(str.charAt(i));
-            getCombs(res, digits, sb);
+        String temp=getChars(digits.charAt(index));
+        for(int i=0;i<temp.length();i++){
+            sb.append(temp.charAt(i));
+            getCombs(res, sb, digits, index+1);
             sb.deleteCharAt(sb.length()-1);
         }
     }
-
     public List<String> letterCombinations(String digits) {
         List<String> res=new ArrayList<>();
-        StringBuilder sb=new StringBuilder("");
-        getCombs(res, digits, sb);
+        StringBuilder sb=new StringBuilder();
+        getCombs(res, sb, digits, 0);
         return res;
     }
 }
