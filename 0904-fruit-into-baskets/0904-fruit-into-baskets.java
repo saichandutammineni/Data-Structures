8d9e3ebb28d2d61c26fc7b7eeebb8d50272407cb
SCT <63411236+saichandutammineni@users.noreply.github.com>
2026-03-07 17:42:39 -0800

    Time: 36 ms (89.75%), Space: 69.1 MB (78.98%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,31 +1,34 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        if(fruits.length<3) return fruits.length;
-        int i=0, j=0, max=0;
-        HashMap<Integer, Integer> hm=new HashMap<>();
-        while(j<fruits.length){
-            if(!hm.containsKey(fruits[j])){
-                if(hm.size()==2){
-                    int min=0, minVal=Integer.MAX_VALUE;
-                    for(Map.Entry<Integer, Integer> entry: hm.entrySet()){
-                        if(entry.getValue()<minVal){
-                            minVal=entry.getValue();
-                            min=entry.getKey();
-                        }
+        if (fruits.length < 3) return fruits.length;
+
+        HashMap<Integer, Integer> lastIndex = new HashMap<>();
+        int max = 0, i = 0;
+
+        for (int j = 0; j < fruits.length; j++) {
+            // update the last seen index of the current fruit
+            lastIndex.put(fruits[j], j);
+
+            // if we have more than 2 fruit types, shrink the window
+            if (lastIndex.size() > 2) {
+                // find the fruit with the smallest last seen index
+                int minIndex = Integer.MAX_VALUE;
+                int fruitToRemove = -1;
+                for (Map.Entry<Integer, Integer> entry : lastIndex.entrySet()) {
+                    if (entry.getValue() < minIndex) {
+                        minIndex = entry.getValue();
+                        fruitToRemove = entry.getKey();
                     }
-                    hm.remove(min);
-                    max=Math.max(max, j-i);
-                    i=minVal+1;
                 }
-                
-                hm.put(fruits[j], j);
+                // remove that fruit and move i to one past its last index
+                lastIndex.remove(fruitToRemove);
+                i = minIndex + 1;
             }
-            else{
-                hm.put(fruits[j], j);
-                
-            }
-            j++;
+
+            // update max window size
+            max = Math.max(max, j - i + 1);
         }
-        return Math.max(max, j-i);
+
+        return max;
     }
 }
