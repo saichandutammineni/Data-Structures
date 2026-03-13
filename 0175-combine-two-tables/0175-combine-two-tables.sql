d55552681c141ec9ae96fe7569a800dd80fbd7f9
SCT <63411236+saichandutammineni@users.noreply.github.com>
2026-03-12 17:34:44 -0700

    Time: 388 ms (66.92%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-SELECT p.firstName, p.lastName, IFNULL(a.city, null) AS city, IFNULL(a.state, null) AS state
+SELECT p.firstName, p.lastName, a.city, a.state
 FROM Person p
 LEFT JOIN Address a ON p.personId=a.personId 
