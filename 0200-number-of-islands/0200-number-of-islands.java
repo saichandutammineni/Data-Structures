eb40487aac2a289565aacac860958d1c56a96800
SCT <63411236+saichandutammineni@users.noreply.github.com>
2026-03-14 18:38:13 -0700

    Time: 254 ms (5.43%), Space: 56.4 MB (5.36%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,31 +1,68 @@
 class Solution {
-    private void bfs(char[][] grid, int i, int j){
-        grid[i][j]=0;
-        if(i-1>=0 && grid[i-1][j]=='1'){
-            bfs(grid, i-1, j);
+    private boolean union(int id1, int id2, int[] parent, int[] rank){
+        int p1=findParent(id1, parent);
+        int p2=findParent(id2, parent);
+        if(p1==p2) return false;
+        else if(rank[p1]<rank[p2]){
+            parent[p1]=p2;
         }
-        if(i+1<grid.length && grid[i+1][j]=='1'){
-            bfs(grid, i+1, j);
+        else if(rank[p2]<rank[p1]){
+            parent[p2]=p1;
         }
-        if(j+1<grid[0].length && grid[i][j+1]=='1'){
-            bfs(grid, i, j+1);
+        else{
+            parent[p2]=p1;
+            rank[p1]++;
         }
-        if(j-1>=0 && grid[i][j-1]=='1'){
-            bfs(grid, i, j-1);
+        return true;
+    }
+
+    private int findParent(int id, int[] parent){
+        if(id!=parent[id]){
+            parent[id]=findParent(parent[id], parent);
         }
+        return parent[id];
     }
 
 
     public int numIslands(char[][] grid) {
-        int count=0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
+        int m=grid.length, n=grid[0].length, count=0;
+        int[] parent=new int[m*n];
+        int[] rank=new int[m*n];
+        //initailizing parent, rank
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int idx=i*n+j;
                 if(grid[i][j]=='1'){
                     count++;
-                    bfs(grid, i, j);
+                    parent[idx]=idx;
+                }
+                else{
+                    parent[idx]=-1; // water or 0
+                }
+                rank[idx]=0;
+            }
+        }
+
+        int[][] dirs=new int[][]{{0,1}, {1,0}};
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='1'){
+                int idx=i*n+j;
+                for(int[] dir: dirs){
+                    int ni=i+dir[0];
+                    int nj=j+dir[1];
+                    System.out.println(ni+"ni"+nj+"nj");
+                    if(ni<m && nj<n && grid[ni][nj]=='1'){
+                        int idx2=ni * n+nj;
+                        if(union(idx, idx2, parent, rank)){
+                            count--;
+                        }
+                    }
+                }
                 }
             }
         }
         return count;
+
     }
 }
