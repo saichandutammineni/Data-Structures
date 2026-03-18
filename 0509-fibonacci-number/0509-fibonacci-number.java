14522438c5980c7954382db5f7cb4a36c60eb328
SCT <63411236+saichandutammineni@users.noreply.github.com>
2026-03-18 01:56:08 -0700

    Time: 0 ms (100%), Space: 41.9 MB (82.14%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -2,9 +2,14 @@ class Solution {
     private int fibo(int curr, int[] dp){
         if(curr==0) return 0;
         if(curr==1) return 1;
-        if(dp[curr]!=-1) return dp[curr];
+        //if(dp[curr]!=-1) return dp[curr];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=curr;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
 
-        dp[curr]=fibo(curr-1, dp)+fibo(curr-2, dp);
+        //dp[curr]=fibo(curr-1, dp)+fibo(curr-2, dp);
         return dp[curr];
     }
     public int fib(int n) {
