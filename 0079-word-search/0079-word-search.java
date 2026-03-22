ce4a7e754f4df5f5d86b23a02e2b23dcb132f2cb
SCT <63411236+saichandutammineni@users.noreply.github.com>
2026-03-21 18:10:01 -0700

    Time: 87 ms (96.22%), Space: 43.2 MB (26.26%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,38 +1,42 @@
 class Solution {
 
-    private boolean dfs(String word, int index, char[][] board, int i, int j, HashSet<List<Integer>> hs){
+    private boolean dfs(String word, int index, char[][] board, int i, int j){
         if(index==word.length()) return true;
-        if(i>0 && board[i-1][j]==word.charAt(index) && !hs.contains(List.of(i-1, j))){
-            hs.add(List.of(i-1,j));
-            if(dfs(word, index+1, board, i-1, j, hs)) return true;
-            else hs.remove(List.of(i-1, j));
+        if(i>0 && board[i-1][j]==word.charAt(index)){
+            char temp=board[i-1][j];
+            board[i-1][j]='&';
+            if(dfs(word, index+1, board, i-1, j)) return true;
+            else board[i-1][j]=temp;
         }
-        if(j>0 && board[i][j-1]==word.charAt(index) && !hs.contains(List.of(i, j-1))){
-            hs.add(List.of(i,j-1));
-            if(dfs(word, index+1, board, i, j-1, hs)) return true;
-            else hs.remove(List.of(i, j-1));
+        if(j>0 && board[i][j-1]==word.charAt(index)){
+            char temp=board[i][j-1];
+            board[i][j-1]='&';
+            if(dfs(word, index+1, board, i, j-1)) return true;
+            else board[i][j-1]=temp;
         }
-        if(i<board.length-1 && board[i+1][j]==word.charAt(index) && !hs.contains(List.of(i+1, j))){
-            hs.add(List.of(i+1,j));
-            if(dfs(word, index+1, board, i+1, j, hs)) return true;
-            else hs.remove(List.of(i+1, j));
+        if(i<board.length-1 && board[i+1][j]==word.charAt(index)){
+            char temp=board[i+1][j];
+            board[i+1][j]='&';
+            if(dfs(word, index+1, board, i+1, j)) return true;
+            else board[i+1][j]=temp;
         }
-        if(j<board[0].length-1 && board[i][j+1]==word.charAt(index)  && !hs.contains(List.of(i, j+1))){
-            hs.add(List.of(i,j+1));
-            if(dfs(word, index+1, board, i, j+1, hs)) return true;
-            else hs.remove(List.of(i, j+1));
+        if(j<board[0].length-1 && board[i][j+1]==word.charAt(index)){
+            char temp=board[i][j+1];
+            board[i][j+1]='&';
+            if(dfs(word, index+1, board, i, j+1)) return true;
+            else board[i][j+1]=temp;
         }
 
         return false;
     }
     public boolean exist(char[][] board, String word) {
-        HashSet<List<Integer>> hs=new HashSet<>();
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
                 if(word.charAt(0)==board[i][j]){
-                    hs.add(List.of(i,j));
-                    if(dfs(word, 1, board, i, j, hs)) return true;
-                    hs.remove(List.of(i,j));
+                    char temp=board[i][j];
+                    board[i][j]='&';
+                    if(dfs(word, 1, board, i, j)) return true;
+                    board[i][j]=temp;
                 }
             }
         }
