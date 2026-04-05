0130a16c209caa365da73bee1a03dae118b77dae
SCT <63411236+saichandutammineni@users.noreply.github.com>
2026-04-04 18:51:17 -0700

    Time: 6 ms (93.57%), Space: 47 MB (15.86%) - LeetHub

---
+++ 1314-matrix-block-sum/1314-matrix-block-sum.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int[][] matrixBlockSum(int[][] mat, int k) {
+        int m=mat.length, n=mat[0].length, pf=0; // prefixSum
+        int[][] dp=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                // pf+=mat[i][j];
+
+                // if(i>0 && j>0 && j<n-1){
+                //     dp[i][j]=pf-dp[i-1][j+1]+dp[i-1][j];
+                // }
+                // else if(j==0 && i>0){
+                //     dp[i][j]=pf-dp[i-1][n-1]+dp[i-1][j];
+                // }
+                // else{
+                //     dp[i][j]=pf;
+                // }
+
+                dp[i][j] = mat[i][j]
+                    + (i > 0 ? dp[i-1][j] : 0)
+                    + (j > 0 ? dp[i][j-1] : 0)
+                    - (i > 0 && j > 0 ? dp[i-1][j-1] : 0);
+            }
+
+        }
+
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         System.out.println(dp[i][j]);
+        //     }
+        // }
+
+        int[][] res=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                // int i_r=i+k < m ? i+k : m-1;
+                // int i_l=i-k-1 >= 0 ? i-k-1 : 0;
+                // int j_down=j+k<n ? j+k : n-1;
+                // int j_up=j-k-1 >=0 ? j-k-1 : 0;
+                int i_r=Math.min(i+k,m-1);
+                int i_l=i-k-1;
+                int j_down=Math.min(j+k , n-1);
+                int j_up=j-k-1;
+
+                res[i][j]=dp[i_r][j_down];
+                if(i_l>=0){
+                    res[i][j]-=dp[i_l][j_down];
+                }
+                if(j_up>=0){
+                    res[i][j]-=dp[i_r][j_up];
+                }
+                if(i_l>=0 && j_up>=0){
+                    res[i][j]+=dp[i_l][j_up];
+                }
+                
+
+            }
+        }
+        return res;
+    }
+}
