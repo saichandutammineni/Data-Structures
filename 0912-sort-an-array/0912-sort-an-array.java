046d06cedebcfc5934f3b9d9b6fc687396a02e9f
SCT <63411236+saichandutammineni@users.noreply.github.com>
2026-04-05 22:59:01 -0700

    Time: 4 ms (99.28%), Space: 73.1 MB (99.35%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,33 +1,16 @@
 class Solution {
-    private int partition(int[] nums, int l, int r){
-        int i=l-1, j=l;
-        int pivot=l+(int)(Math.random()*(r-l+1));
-        int t=nums[r];
-        nums[r]=nums[pivot];
-        nums[pivot]=t;
-
-        for(; j<=r-1;j++){
-            if(nums[j]<nums[r]){
-                i++;
-                int temp=nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
+    public int[] sortArray(int[] nums) {
+        int[] freq=new int[100001];
+        for(int i:nums){
+            freq[i+50000]++;
+        }
+        int k=0;
+        for(int i=0;i<100001;i++){
+            while(freq[i]>0){
+                nums[k++]=i-50000;
+                freq[i]--;
             }
         }
-        t=nums[i+1];
-        nums[i+1]=nums[r];
-        nums[r]=t;
-        return i+1;
-    }
-    private void quickSort(int[] nums, int l, int r){
-        if(l<r){
-        int pi=partition(nums, l, r);
-        quickSort(nums, l, pi-1);
-        quickSort(nums, pi+1, r);
-        }
-    }
-    public int[] sortArray(int[] nums) {
-        quickSort(nums, 0, nums.length-1);
         return nums;
     }
 }
