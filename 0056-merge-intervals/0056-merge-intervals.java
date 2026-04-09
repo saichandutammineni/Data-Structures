e448169edad29f4ede82d2529ae76fce6ca7f39c
SCT <63411236+saichandutammineni@users.noreply.github.com>
2026-04-08 23:55:26 -0700

    Time: 8 ms (91.28%), Space: 49.1 MB (46.09%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,24 +1,21 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a,b)-> Integer.compare(a[0], b[0]));
         List<int[]> res=new ArrayList<>();
-        Arrays.sort(intervals, (a,b)-> {
-            return Integer.compare(a[0], b[0]);
-        });
-        int l=intervals[0][0], r=intervals[0][1];
 
-        
+        int start=intervals[0][0], end=intervals[0][1];
         for(int i=1;i<intervals.length;i++){
-            if(r>=intervals[i][0]){
-                r=Math.max(r, intervals[i][1]);
+            if(intervals[i][0]<=end){
+                end=Math.max(intervals[i][1], end);
             }
             else{
-                res.add(new int[]{l, r});
-                l=intervals[i][0];
-                r=intervals[i][1];
+                res.add(new int[]{start, end});
+                start=intervals[i][0];
+                end=intervals[i][1];
             }
         }
+        res.add(new int[]{start, end});
 
-        res.add(new int[]{l, r});
         return res.toArray(new int[res.size()][]);
     }
 }
