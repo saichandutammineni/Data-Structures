26d196153ee45fac25a7f76a9166aa67ed236a1d
SCT <63411236+saichandutammineni@users.noreply.github.com>
2026-05-31 13:50:03 -0700

    Time: 3 ms (97.44%), Space: 67 MB (12.73%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.java
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.java
@@ -1,16 +1,12 @@
 class Solution {
     public int maxAbsoluteSum(int[] nums) {
-        int max=0, min=0, res=0;
+        int maxPrefix=0, minPrefix=0, prefix=0;
 
-        for(int i=0;i<nums.length;i++){
-            
-            max=Math.max(Math.max(0,nums[i]), max+nums[i]);
-            
-            min=Math.min(Math.min(nums[i],0), min+nums[i]);
-
-
-            res=Math.max(res, Math.max(max, min*-1));
+        for(int i:nums){
+            prefix+=i;
+            maxPrefix=Math.max(prefix, maxPrefix);
+            minPrefix=Math.min(prefix, minPrefix);
         }
-        return res;
+        return maxPrefix-minPrefix;
     }
 }
