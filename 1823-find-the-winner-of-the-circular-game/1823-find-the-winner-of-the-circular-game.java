dd97d28e79f4888b3298c7883ade9fc96cdc2ce9
SCT <63411236+saichandutammineni@users.noreply.github.com>
2026-06-09 18:17:21 -0700

    Time: 0 ms (100%), Space: 42.1 MB (82.74%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -4,7 +4,7 @@ class Solution {
         // Approach 2: Queue, popping and pushing (moving) k-1 nodes to end then deleting 1 node. O(n*k)
 
         // Approach 3: given below
-        int temp=0; // index of the winner node will always be 0. 
+        int temp=0; // index of the winner node will always be 0 when size is 1. 
         for(int i=2; i<=n;i++){
             temp=(temp+k)%i; // i is the size of problem set while constructing from size 2 to n
 
