a5f6151dfab3e8a3e5b487ddd042180414052c61
SCT <63411236+saichandutammineni@users.noreply.github.com>
2026-07-01 19:56:45 -0700

    Time: 2 ms (97%), Space: 43.5 MB (93%) - LeetHub

--- 2591-distribute-money-to-maximum-children/2591-distribute-money-to-maximum-children.java
+++ 2591-distribute-money-to-maximum-children/2591-distribute-money-to-maximum-children.java
@@ -1,25 +1,25 @@
 class Solution {
     public int distMoney(int money, int children) {
         if(money<children) return -1;
-        int count=0;
         // give 1 dollar to each child, satisfies req 2
         money-=children;
         // give 7 dollars to max. children possible
-        while(money>=7 && children>0){
-            money-=7;
-            count++;
-            children--;
-        }
+        int childrenwith8=Math.min(children, money/7);
+
+        //remaining children without 8
+        children-=childrenwith8;
+        //left money
+        money-=(7*childrenwith8);
 
         
         if(money==3 && children==1){
             // case to handle: if only 1 child left and 3 dollars of money left
-            count--;
+            childrenwith8--;
         }
         else if(money>0 && children==0){
             // give leftover money to any one child
-            count--;
+            childrenwith8--;
         }
-        return count;
+        return childrenwith8;
     }
 }
