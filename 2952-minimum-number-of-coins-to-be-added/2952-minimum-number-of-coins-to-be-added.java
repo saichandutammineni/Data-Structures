92f6562019515ea1dc3d4c4779a3d3f34825e6cc
SCT <63411236+saichandutammineni@users.noreply.github.com>
2026-07-03 01:36:36 -0700

    Time: 26 ms (96.15%), Space: 78.7 MB (59.62%) - LeetHub

--- 2952-minimum-number-of-coins-to-be-added/2952-minimum-number-of-coins-to-be-added.java
+++ 2952-minimum-number-of-coins-to-be-added/2952-minimum-number-of-coins-to-be-added.java
@@ -2,7 +2,7 @@ class Solution {
     public int minimumAddedCoins(int[] coins, int target) {
         Arrays.sort(coins);
 
-        int obt=1, i=0, count=0, cumm=0;
+        int obt=1, i=0, count=0, cumm=0; // https://www.youtube.com/watch?v=DQM_CfD4iJk
         while(obt<=target){
             if(i<coins.length && coins[i]<=obt){
                 cumm+=coins[i];
