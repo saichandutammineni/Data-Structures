572ae24639077a7d8f8120faa9ef729e42b73e93
SCT <63411236+saichandutammineni@users.noreply.github.com>
2026-07-03 19:21:57 -0700

    Time: 3 ms (100%), Space: 60.9 MB (5.03%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -1,19 +1,50 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
-        Arrays.sort(people);
+        int[] freq=new int[limit+1];
 
-        int i=0, j=people.length-1, count=0;
+        for(int i:people){
+            freq[i]++;
+        }
 
-        while(i<=j){
-            if(people[i]+people[j]<=limit){
-                i++;
-                j--;
+        int l=1,r=freq.length-1, count=0;
+        while(l<=r){
+            while(l<=r && freq[r]==0){
+                r--;
+            }
+            while(l<=r && freq[l]==0){
+                l++;
+            }
+
+            if(l==r){
+                if(l+r<=limit){
+                    count+=(freq[l]+1)/2;
+                }
+                else{
+                    count+=freq[l];
+                }
+                l++;
+            }
+            else if(l+r<=limit){
+                if(freq[l]<freq[r]){
+                    freq[r]-=freq[l];
+                    
+                count+=freq[l];
+                freq[l]=0;
+                l++;
+                }
+                else{
+                    count+=freq[r];
+                    freq[l]-=freq[r];
+                    freq[r]=0;
+                r--;
+                }
+
             }
             else{
-                
-                j--;
-            }
-            count++;
+                count+=freq[r];
+                freq[r]=0;
+                r--;
+            }   
         }
         return count;
     }
