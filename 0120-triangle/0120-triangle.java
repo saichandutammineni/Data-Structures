19bc5bd983133e584252a61c218b7c74f6796125
SCT <63411236+saichandutammineni@users.noreply.github.com>
2026-08-01 14:34:17 -0700

    Time: 4 ms (41.87%), Space: 45.3 MB (75.78%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,21 +1,29 @@
 class Solution {
 
     private void findWays(List<List<Integer>> triangle, int[][] dp){
-        for(int i=triangle.size()-1;i>=0;i--){
+        int n=triangle.size();
+        for(int i=n-1;i>=0;i--){
+            dp[1][i]=triangle.get(n-1).get(i);
+        }
+
+
+
+        for(int i=n-2;i>=0;i--){
             for(int j=triangle.get(i).size()-1;j>=0;j--){
-                int a=0, b=0;
-                if(i+1<triangle.size()){
-                    a=dp[i+1][j];
-                    if(j+1<triangle.get(i+1).size()){
-                        b=dp[i+1][j+1];
-                    }
-                }
-                dp[i][j]=triangle.get(i).get(j)+Math.min(a,b);
+                dp[0][j]=triangle.get(i).get(j)+Math.min(dp[1][j], dp[1][j+1]);
+            }
+            
+            int t=0;
+            for(int k: dp[0]){
+                dp[1][t++]=k;
             }
         }
     }
     public int minimumTotal(List<List<Integer>> triangle) {
-        int[][] dp=new int[triangle.size()+1][triangle.size()+1];
+        if(triangle.size()==1){
+            return triangle.get(0).get(0);
+        }
+        int[][] dp=new int[2][triangle.size()+1];
         findWays(triangle, dp);
         return dp[0][0];
     }
