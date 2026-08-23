95bfdd03e6d46d6f91cdce3f175f98dd43548c13
SCT <63411236+saichandutammineni@users.noreply.github.com>
2026-08-22 18:32:29 -0700

    Time: 5 ms (99.81%), Space: 43.1 MB (78.53%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,22 +1,15 @@
 class Solution {
-    private int solve(int amount, int[][] dp, int[] coins, int index){
-        if(amount==0) return 1;
-        if(index==coins.length || amount<0) return 0;
-
-        if(dp[amount][index]!=-1) return dp[amount][index];
-        int sum=0;
-        
-        sum+=solve(amount-coins[index], dp, coins, index);
-        sum+=solve(amount, dp, coins, index+1);
-
-        return dp[amount][index]=sum;
-    }
     public int change(int amount, int[] coins) {
-        int[][] dp=new int[amount+1][coins.length];
-        for(int i=1;i<dp.length;i++)
-            Arrays.fill(dp[i], -1);
-        //dp[0][0]=1;
         
-        return solve(amount, dp, coins, 0);
+        int[] dp=new int[amount+1];
+
+        dp[0]=1;
+
+        for(int coin: coins){
+            for(int j=coin;j<=amount;j++){
+                dp[j]+=dp[j-coin];
+            }
+        }
+        return dp[amount];
     }
 }
