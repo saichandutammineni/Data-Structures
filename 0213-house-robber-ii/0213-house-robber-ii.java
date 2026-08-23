dca3ac56f2531be0bfed81af83d9fdc8fb2e2e9d
SCT <63411236+saichandutammineni@users.noreply.github.com>
2026-08-23 16:53:32 -0700

    Time: 0 ms (100%), Space: 42.7 MB (60.75%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,23 +1,19 @@
 class Solution {
-    private int solve(int[] dp, int[] nums, int index){
-        if(index>=nums.length) return 0;
-        if(dp[index]!=-1) return dp[index];
-
-        return dp[index]=Math.max(nums[index]+solve(dp, nums, index+2), solve(dp, nums, index+1));
-    }
     public int rob(int[] nums) {
-        
         int n=nums.length;
         if(n==1) return nums[0];
-        int[] dp=new int[n];
-        
-        Arrays.fill(dp, -1);
-        
-        int temp=nums[n-1];
-        
-        int a=solve(dp, nums, 1);
-        nums[n-1]=0;
-        Arrays.fill(dp, -1);
-        return Math.max(a, nums[0]+solve(dp, nums, 2));
+        return Math.max(solve(nums, 0, n-2), solve(nums, 1, n-1));
+    }
+
+    private int solve(int[] nums, int start, int end){
+        if(end==start) return nums[start];
+        int[] dp=new int[nums.length];
+        dp[end]=nums[end];
+        dp[end-1]=Math.max(dp[end],nums[end-1]);
+        for(int i=end-2;i>=start;i--){
+            dp[i]=Math.max(nums[i]+dp[i+2], dp[i+1]);
+        }
+
+        return dp[start];
     }
 }
