1e4678a42702964f685994dd441b7abd9c5ecf56
SCT <63411236+saichandutammineni@users.noreply.github.com>
2026-08-23 17:12:14 -0700

    Time: 0 ms (100%), Space: 41.8 MB (96.24%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,12 +1,18 @@
 class Solution {
-    public int uniquePaths(int m, int n) {
-        int N = m + n - 2;
-        int k = Math.min(m - 1, n - 1);
+    private int solve(int[][] dp, int x, int y, int m, int n){
+        if(x==m-1 && y==n-1) return 1;
+        if(x>m-1) return 0;
+        if(y>n-1) return 0;
 
-        long res = 1;
-        for (int i = 1; i <= k; i++) {
-            res = res * (N - k + i) / i;
+        if(dp[x][y]!=-1) return dp[x][y];
+        return dp[x][y]=solve(dp, x+1, y, m, n)+solve(dp, x, y+1, m, n);
+    }
+    public int uniquePaths(int m, int n) {
+        int[][] dp=new int[m][n];
+        for(int i=0;i<m;i++){
+            Arrays.fill(dp[i], -1);
         }
-        return (int) res;
+
+        return solve(dp, 0, 0, m, n);
     }
 }
