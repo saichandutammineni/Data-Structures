c7b43cd9ce4d66adf298145768e6690c90eee718
SCT <63411236+saichandutammineni@users.noreply.github.com>
2026-08-24 21:33:54 -0700

    Time: 7 ms (51.41%), Space: 61.2 MB (96.63%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -1,36 +1,30 @@
 class Solution {
-    private int solve(int[][] mat, int[][] dp, int x, int y){
-        if(x>=mat.length || y>=mat[0].length) return 0;
-        if(dp[x][y]!=-1) return dp[x][y];
-
-        
-        // if(mat[x][y]==0){
-        //     return dp[x][y]=0;
-        // }
-        // else{
-            return dp[x][y]=1+Math.min(solve(mat, dp, x+1, y), Math.min(solve(mat, dp, x, y+1), solve(mat, dp, x+1, y+1)));
-        // }
-        
-    }
+    
     public int countSquares(int[][] matrix) {
-        int[][] dp=new int[matrix.length][matrix[0].length];
+        int[][] dp=new int[matrix.length+1][matrix[0].length+1];
 
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(matrix[i][j]==0) dp[i][j]=0;
-                else dp[i][j]=-1;
-            }
-            
-        }
+        // for(int i=0;i<matrix.length;i++) dp[i][0]=0;
+        // for(int j=0;j<matrix[0].length;j++) dp[0][j]=0;
 
-        
         int res=0;
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
-                
-                res+=solve(matrix, dp, i, j);
+                if(matrix[i][j]==0) dp[i+1][j+1]=0;
+                else{
+                    dp[i+1][j+1]=1+Math.min(dp[i][j+1], Math.min(dp[i+1][j], dp[i][j]));
+                    res+=dp[i+1][j+1];
+                }
             }
         }
+
+        
+        
+        // for(int i=0;i<matrix.length;i++){
+        //     for(int j=0;j<matrix[0].length;j++){
+                
+        //         res+=solve(matrix, dp, i, j);
+        //     }
+        // }
         return res;
     }
 }
