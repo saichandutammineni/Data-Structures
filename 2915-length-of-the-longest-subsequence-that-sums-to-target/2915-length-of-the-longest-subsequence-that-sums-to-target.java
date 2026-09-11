62c44eb54b99ab7b58051654510ed526c54febe0
SCT <63411236+saichandutammineni@users.noreply.github.com>
2026-09-10 17:11:09 -0700

    Time: 239 ms (69.02%), Space: 50.6 MB (53.85%) - LeetHub

--- 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.java
+++ 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.java
@@ -1,28 +1,30 @@
 class Solution {
-    private int solve(int[][] dp, List<Integer> nums, int index, int target){
-        if(target==0){
-            return 0;
-        }
-        else if(index==nums.size()) return -2;
-        else if(dp[index][target]!=-1) return dp[index][target];
-
-        int t=solve(dp,nums, index+1,target);
-        int t1=-2;
-        if(target>=nums.get(index)){
-            int res=solve(dp, nums, index+1, target-nums.get(index));
-            if(res!=-2)
-                t1=res+1;
-        }
-        return dp[index][target]=Math.max(t1, t);
-    }
     public int lengthOfLongestSubsequence(List<Integer> nums, int target) {
-        int[][] dp=new int[nums.size()][target+1];
+        int n=nums.size();
+        int[][] dp=new int[n][target+1];
 
-        for(int i=0;i<nums.size();i++) Arrays.fill(dp[i], -1);
-        // -1 -> uncomputed, -2 -> impossible state
+        for(int j=1;j<target+1;j++){
+            if(j==nums.get(0) && dp[0][j-nums.get(0)]!=-1){
+                dp[0][j]=1+dp[0][j-nums.get(0)];
+            }
+            else{
+                dp[0][j]=-1;
+            }
+        }
 
-        int ans=solve(dp,nums, 0, target);
-        return ans!=-2 ? ans : -1;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<target+1;j++){
+                int temp=dp[i-1][j];
+                if(j>=nums.get(i)){
+                    if(dp[i-1][j-nums.get(i)]!=-1)
+                    temp=Math.max(temp, 1+dp[i-1][j-nums.get(i)]);
+                    
+                }
+                dp[i][j]=temp;
+            }
+        }
+
+        return dp[n-1][target];
         
     }
 }
