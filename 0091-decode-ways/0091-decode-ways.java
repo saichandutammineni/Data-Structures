411a008cadb11d342ebae0d05a502bf3bf58f1d1
SCT <63411236+saichandutammineni@users.noreply.github.com>
2026-09-20 01:08:46 -0700

    Time: 0 ms (100%), Space: 42.7 MB (93.45%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,54 +1,30 @@
 class Solution {
-    private int solve(String s, int[] dp, int index){
-        if(index==s.length()){
+    private int solve(String s, Integer[] dp, int index, int val){
+        if(index==s.length() && val>=0 && val<=26){
             return 1;
         }
-        if(dp[index]!=-1){
+        if(val>26){
+            return 0;
+        }
+        if(dp[index]!=null){
             return dp[index];
         }
 
         if(s.charAt(index)=='0'){
-            // if(index>0 && (s.charAt(index-1)=='1' || s.charAt(index-1)=='2')){
-            //     return dp[index]=solve(s, dp, index+1);
-            // }
-            // else{
-                return dp[index]=0;
-            // }
+            return dp[index]=0;
         }
 
-        if(s.charAt(index)=='1'){
-            int temp=0;
-            
-            if(index+1<s.length() && s.charAt(index+1)=='0'){
-                temp=solve(s, dp, index+2);
-            }
-            else if(index+1<s.length() && s.charAt(index+1)!='0'){
-                temp=solve(s, dp, index+1)+solve(s, dp, index+2);
-            }
-            else{
-                temp=solve(s, dp, index+1);
-            }
-            return dp[index]=temp;
+        int first=s.charAt(index)-'0';
+        int temp=solve(s, dp, index+1, first);
+        if(index+1<s.length()){
+
+            int second=first*10 + s.charAt(index+1)-'0';
+            temp+=solve(s, dp, index+2, second);
         }
-        if(s.charAt(index)=='2'){
-            HashSet<Character> set=new HashSet<>(Arrays.asList('1', '2', '3', '4','5', '6'));
-            if(index+1<s.length() && set.contains(s.charAt(index+1))){
-                return dp[index]=solve(s, dp, index+1)+solve(s, dp, index+2);
-            }
-            else if(index+1<s.length() && s.charAt(index+1)=='0'){
-                return dp[index]=solve(s, dp, index+2);
-            }
-            else{
-                return dp[index]=solve(s, dp, index+1);
-            }
-        }
-        return dp[index]=solve(s, dp, index+1);
+        return dp[index]=temp;
     }
     public int numDecodings(String s) {
-        //StringBuilder sb=new StringBuilder("");
-
-        int[] dp=new int[s.length()];
-        Arrays.fill(dp, -1);
-        return solve(s, dp, 0);
+        Integer[] dp=new Integer[s.length()];
+        return solve(s, dp, 0, 0);
     }
 }
