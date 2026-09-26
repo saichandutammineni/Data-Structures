b6f052ce6967821b98f105624972cdd84a1737e6
SCT <63411236+saichandutammineni@users.noreply.github.com>
2026-09-26 02:03:28 -0700

    Time: 2 ms (100%), Space: 46.3 MB (34.86%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,23 +1,33 @@
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int n=nums.length;
-        int[][] dp=new int[2][n+1];
-
-        for(int i=n-1;i>=0;i--){
-            for(int j=0;j<=i;j++){
-                if(j==0){
-                    dp[0][j]=Math.max(dp[1][i+1]+1, dp[1][j]);
-                }
-                else{
-                if(nums[i]>nums[j-1])
-                dp[0][j]=Math.max(dp[1][i+1]+1, dp[1][j]);
-                else
-                dp[0][j]=dp[1][j];
-                }
+    private int binaryS(int[] nums, int l, int r, int target){
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]==target){
+                return mid;
             }
-            dp[1]=dp[0];
+            else if(nums[mid]>target){
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
         }
+        return l;
+    }
+    public int lengthOfLIS(int[] nums) {
+        int[] res=new int[nums.length];
 
-        return dp[0][0];
+        int l=-1;
+        res[++l]=nums[0];
+        for(int n: nums){
+            if(res[l]<n){
+                res[++l]=n;
+            }
+            else{
+                int id=binaryS(res, 0, l, n);
+                res[id]=n;
+            }
+        }
+        return l+1;
     }
 }
