a8b8b4146dcae04d02b720925c93005cc703319f
SCT <63411236+saichandutammineni@users.noreply.github.com>
2026-09-25 19:43:35 -0700

    Time: 87 ms (25.04%), Space: 237.1 MB (18.21%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -4,7 +4,7 @@ class Solution {
         int[][] dp=new int[n+1][n+1];
 
         for(int i=n-1;i>=0;i--){
-            for(int j=0;j<n;j++){
+            for(int j=0;j<=i;j++){
                 if(j==0){
                     dp[i][j]=Math.max(dp[i+1][i+1]+1, dp[i+1][j]);
                 }
